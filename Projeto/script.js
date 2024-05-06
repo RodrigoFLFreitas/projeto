2a49d2d1b8edcd69d10f77d2ce289779373994e1
RodrigoFLFreitas <129107800+RodrigoFLFreitas@users.noreply.github.com>
2024-05-06 15:34:33 -0300

    :tada:

---
+++ Projeto/script.js
@@ -0,0 +1,16 @@
+function ToggleMode() {
+  const html = document.documentElement
+  html.classList.toggle('light')
+  
+  const img = document.querySelector("#profile img")
+  // substituir a imagem 
+  if(html.classList.contains('light')){
+  
+    img.setAttribute("src", "./assets/avatar-light.png")
+  
+  } else {
+  
+    img.setAttribute("src" , "./assets/avatar.png")
+  
+  }
+}
